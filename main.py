c1e7b91df8f1361448967d696016e85879abe9c8
mr_goldenman <vasya.chaukin.97@mail.ru>
2022-04-13 19:31:52 +0500

    вернемся к старому

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
-import json
-
-import vk
+import vk_api
 import requests
 import datetime
-from flask import Flask
-
-app = Flask(__name__)
+from vk_api.longpoll import VkLongPoll, VkEventType
 now = datetime.datetime.now()
-
+session = requests.Session()
 
 token='d43183177e20c8307a7815180aaef13a8b56a0b7e63b06697001cae96f98248680a5dbf2e04969b047aac'
-group_id = 212652278
-
-@app.route("/", methods=['POST'])
-def processing():
-    data = json.loads(requests.data)
-    session = requests.Session()
-    api = vk.API(session)
-    api.messages.send(access_token=token, chat_id=group_id,message="asdiuhsdfhiusdaihu", random_id=0)
-
-    return "a322dfc1"
 
 
+def write_msg(user_id, message):
+    vk.method('messages.send', {'user_id': user_id, 'message': message})
+
+# Авторизуемся как сообщество
+vk = vk_api.VkApi(token=token)
+
+# Работа с сообщениями
+longpoll = VkLongPoll(vk)
 
 # Основной цикл
-# for event in longpoll.listen():
-#     if not event.from_chat:
-#         d =datetime.datetime.now().hour
-#         if datetime.datetime.today().weekday() == 2 and (d ==14 or d == 18 or d ==20):
-#             write_msg(event.user_id, "завтра ОБЖ! всем принести тетрадки, иначе не будем смотреть видео, а будем писать в тетрадках!")
-#         # Если пришло новое сообщение
+for event in longpoll.listen():
 
+    # Если пришло новое сообщение
+    if event.type == VkEventType.MESSAGE_NEW:
 
+        # Если оно имеет метку для меня( то есть бота)
+        if event.to_me:
+
+            # Сообщение от пользователя
+            request = event.text
+
+            # Каменная логика ответа
+            if request == "привет":
+                write_msg(event.user_id, "Хай")
+            elif request == "пока":
+                write_msg(event.user_id, "Пока((")
+            else:
+                write_msg(event.user_id, "Не понял вашего ответа...")
