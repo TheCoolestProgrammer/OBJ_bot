8ae8ec0bbc91d71aa377e3a3f33849bb15fac7d5
TheCoolestProgrammer <81899758+TheCoolestProgrammer@users.noreply.github.com>
2022-04-13 17:08:08 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -4,24 +4,35 @@ import datetime
 from vk_api.longpoll import VkLongPoll, VkEventType
 now = datetime.datetime.now()
 session = requests.Session()
-vk_session = vk_api.VkApi(token='d43183177e20c8307a7815180aaef13a8b56a0b7e63b06697001cae96f98248680a5dbf2e04969b047aac')
 
-event=VkEventType(vk_session)
-
-longpoll = VkLongPoll(vk_session)
-vk = vk_session.get_api()
-def main():
-    try:
-        vk_session.auth(token_only=True)
-        vk.messages.send(
-            user_id=event.user_id,
-            message= str(now.strftime("%H:%M"))
-        )
-    except vk_api.AuthError as error_msg:
-        print(error_msg)
-        return
-
-if __name__ == '__main__':
-    main()
+token='d43183177e20c8307a7815180aaef13a8b56a0b7e63b06697001cae96f98248680a5dbf2e04969b047aac'
 
 
+def write_msg(user_id, message):
+    vk.method('messages.send', {'user_id': user_id, 'message': message})
+
+# Авторизуемся как сообщество
+vk = vk_api.VkApi(token=token)
+
+# Работа с сообщениями
+longpoll = VkLongPoll(vk)
+
+# Основной цикл
+for event in longpoll.listen():
+
+    # Если пришло новое сообщение
+    if event.type == VkEventType.MESSAGE_NEW:
+
+        # Если оно имеет метку для меня( то есть бота)
+        if event.to_me:
+
+            # Сообщение от пользователя
+            request = event.text
+
+            # Каменная логика ответа
+            if request == "привет":
+                write_msg(event.user_id, "Хай")
+            elif request == "пока":
+                write_msg(event.user_id, "Пока((")
+            else:
+                write_msg(event.user_id, "Не понял вашего ответа...")
